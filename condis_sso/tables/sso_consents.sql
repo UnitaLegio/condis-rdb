ce0d39eef10c1de88251d6f2cdb885d0ee9c2f09
Max Pestov <pestov.maxik@gmail.com>
2024-01-13 23:21:57 +0400

    Errors fixes and logout table

--- condis_sso/tables/sso_consents.sql
+++ condis_sso/tables/sso_consents.sql
@@ -3,7 +3,7 @@ create table sso_consents
     id       int auto_increment
         primary key,
     user_id  int                                not null references condis_sso.sso_users,
-    scope_id int                                not null,
+    scope_id int                                not null references condis_sso.sso_client_scopes,
     grant_date  datetime default CURRENT_TIMESTAMP not null
 );
 
